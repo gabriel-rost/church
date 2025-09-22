ae790cc737bf732f599c2e7848c3c9934ff34c79
Gabriel Rost <gabrielrost62@gmail.com>
2025-09-21 22:08:42 -0300

    Implement post listing and detail views with templates; update URL patterns and settings for template directory.

--- church_project/church_app/urls.py
+++ church_project/church_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="post_list"),
+    path("<int:post_id>/", views.post, name="post_detail"),
 ]
--- church_project/church_app/views.py
+++ church_project/church_app/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Post
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    posts = Post.objects.all().order_by("-date")
+    return render(request, "church_app/post_list.html", {"posts": posts})
+
+def post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    # comentários principais (parent = None)
+    comments = post.comments.filter(parent__isnull=True)
+    return render(request, "church_app/post_detail.html", {
+        "post": post,
+        "comments": comments
+    })
